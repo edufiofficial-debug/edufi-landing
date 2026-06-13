206a5f3971338966da44890b71e77c2d7a5982fa
Ajay kaul <ajaykaul@Ajays-MacBook-Air-2.local>
2026-06-13 11:23:11 +0530

    SEO improvements, sitemap and Search Console setup

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -43,8 +43,8 @@ const tests = [
     topic: "Division Tricks",
     duration: "10 Minutes",
     questions: "20 Questions",
-    link: "#",
-    status: "coming",
+    link: "#https://web.examin8.com/testpaper/assessment/detail/2820670/442751781329633",
+    status: "live",
   },
   {
     classNo: "Class 06",
